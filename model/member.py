acce7bcfcc61e8eefa86ec6582fbce73561df241
Russ Wolf <russ@hirepirates.com>
2015-12-15 18:18:06 +0530

    Added a role field to members

--- model/member.py
+++ model/member.py
@@ -3,3 +3,4 @@ from google.appengine.ext import db
 class Member(db.Model):
     name = db.StringProperty(indexed=False)
     email = db.StringProperty(indexed=True)
+    role = db.StringProperty(indexed=True)
