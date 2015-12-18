891172508799a7a18236c2d8a4f1584b67508c33
Russ Wolf <russ@hirepirates.com>
2015-12-18 12:38:18 +0530

    Laid down the framework for work order management

--- handlers/rest/work_order.py
+++ handlers/rest/work_order.py
@@ -2,15 +2,49 @@ import webapp2
 import urllib2
 import json
 from model.work_order import WorkOrder
+from model.store import Store
+from model.appliance import Appliance
 from handlers.web.web_request_handler import WebRequestHandler
+from auth import login_required
 
 class CreateWorkOrderHandler(WebRequestHandler):
+    def does_user_own_appliance(self):
+        email = self.session['email']
+        role = self.session['role']
+        store = Store.all().filter(role + ' =', email).get()
+        appliance = Appliance.get_by_id(long(self['appliance']))
+        if appliance.store.key().id() == store.key().id():
+            return True
+        return False
+
     @login_required
     def get(self):
-        
-        wo = WorkOrder()
-        wo.update_state({'appliance':self['appliance'], 'provider':self['provider']})
+        ret_val = {}
+        if(self.does_user_own_appliance()):
+            wo = WorkOrder()
+            wo.update_state({'appliance':self['appliance'], 'provider':self['provider']})
+            ret_val = {'status':'success','work_order_id':wo.key().id()}
+        else:
+            ret_val = {'status':'error','message':'User does not own the appliance'}
+        self.write(json.dumps(ret_val))
+
+class UpdateWorkOrderHandler(WebRequestHandler):
+    @login_required
+    def get(self):
+        wo = WorkOrder.get_by_id(long(self['work_order']))
+        ret_val = {}
+        params = {}
+        if self['params']:
+            params_str = self['params']
+            entry_strs = params_str.split(';')
+            for entry_str in entry_strs:
+                entry = entry_str.split(':')
+                params[entry[0]] = entry[1]
+        wo.update_state(params)
+        ret_val = {'status':'success','work_order_id':wo.key().id()}
+        self.write(json.dumps(ret_val))
 
 app = webapp2.WSGIApplication([
-    ('/rest/work_order/create', CreateWorkOrderHandler)
+    ('/rest/work_order/create', CreateWorkOrderHandler),
+    ('/rest/work_order/update', UpdateWorkOrderHandler)
 ])
--- model/work_order.py
+++ model/work_order.py
@@ -9,17 +9,38 @@ class WorkOrderHistory(db.Model):
 class WorkOrder(db.Model):
     appliance = db.StringProperty(indexed=True)
     provider = db.StringProperty(indexed=True)
-    history = db.ListProperty(str)
-    curr_state = db.IntProperty(indexed=True)
-    state_idx = -1
+    history = db.ListProperty(long)
+    curr_state = db.StringProperty(indexed=True)
+
+    def create_wo_history(self, details):
+        woh = WorkOrderHistory()
+        if details:
+            woh.details = details
+        woh.put()
+        self.history.append(woh.key().id())
 
     def update_state(self, params):
-    	if state_idx == -1:
-    		state_idx += 1
-    		if isinstance(work_order_states[state_idx], str):
-    			curr_state = work_order_states[state_idx]
-    		appliance = params['appliance']
-    		provider = params['provider']
-    		woh = WorkOrderHistory()
-    		history = [woh]
+    	if not self.curr_state:
+            self.curr_state = work_order_states[0]
+            self.appliance = params['appliance']
+            self.provider = params['provider']
+            self.create_wo_history(None)
+        elif self.curr_state == 'CREATED':
+            self.curr_state = work_order_states[work_order_states.index(self.curr_state) + 1]
+            self.create_wo_history(params['estimate'])
+        elif self.curr_state == 'ESTIMATED':
+            if params['approval'] == '1':
+                self.curr_state = 'APPROVED'
+            else:
+                self.curr_state = 'DISAPPROVED'
+            self.create_wo_history(None)
+        elif self.curr_state == 'APPROVED' or self.curr_state == 'DISAPPROVED':
+            self.curr_state = work_order_states[work_order_states.index(["APPROVED", "DISAPPROVED"]) + 1]
+            self.create_wo_history(None)
+        elif self.curr_state == 'PROVIDER_CHECKED_IN':
+            self.curr_state = work_order_states[work_order_states.index('PROVIDER_CHECKED_IN') + 1]
+            self.create_wo_history(None)
+        elif self.curr_state == 'PROVIDER_CHECKED_OUT':
+            self.curr_state = work_order_states[work_order_states.index('PROVIDER_CHECKED_OUT') + 1]
+            self.create_wo_history(params['notes'])
     	self.put()
