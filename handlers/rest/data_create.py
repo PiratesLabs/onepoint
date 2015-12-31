6e0f5daa2bcd24febd1928844962d01ce36624fc
Russ Wolf <russ@hirepirates.com>
2015-12-31 14:45:24 +0530

    Finished airtable data pull

--- handlers/rest/data_create.py
+++ handlers/rest/data_create.py
@@ -14,6 +14,13 @@ from google.appengine.ext import db
 from google.appengine.ext import deferred
 from google.appengine.api import urlfetch
 
+def clear_datastore():
+    models = [Member, Store, Appliance, Provider, WorkOrder, WorkOrderHistory]
+    for model in models:
+        q = model.all(keys_only=True)
+        entries = q.fetch(100)
+        db.delete(entries)
+
 class StoreCreationHandler(WebRequestHandler):
     def post(self):
         loc = self['location'].split(',')
@@ -33,12 +40,14 @@ class ApplianceCreationHandler(WebRequestHandler):
 
 class TestDataCreationHandler(WebRequestHandler):
     def create_users(self):
-        Member(key_name='niranjan.salimath@gmail.com', name='Rajiv Owner', role='owner', phone='(617)-840-0716').put()
-        Member(key_name='ranju@hirepirates.com', name='Rajiv Manager', role='manager', phone='(617)-840-0716').put()
-        Member(key_name='ranju@beagleslabs.com', name='Ranju Provider', role='provider', phone='(617)-840-0716').put()
+        owner = Member(key_name='rsalimath@gmail.com', name='Rajiv Owner', role='owner', phone='(617)-840-0716')
+        manager = Member(key_name='rajiv@hirepirates.com', name='Rajiv Manager', role='manager', phone='(617)-840-0716')
+        owner.put()
+        manager.put()
+        return [owner, manager]
 
-    def create_stores(self):
-        store = Store(name="Store1", location=db.GeoPt(40.7131116,-74.015359), manager="rajiv@hirepirates.com", owner="rsalimath@gmail.com", address="40 Harrison Street, New York, NY, United States")
+    def create_stores(self, manager, owner):
+        store = Store(name="Store1", location=db.GeoPt(40.7131116,-74.015359), manager=manager.key().name(), owner=owner.key().name(), address="40 Harrison Street, New York, NY, United States")
         store.put()
         return [store]
 
@@ -51,12 +60,11 @@ class TestDataCreationHandler(WebRequestHandler):
             Appliance(name="Oven2", store=store, manufacturer="Frymaster", model="HV104", serial_num="EGY1909334569", last_repair_date="7/2/2015", installed_on="6/1/2010", warranty="Expired 6/1/2013").put()
             Appliance(name="Oven3", store=store, manufacturer="Frymaster", model="O103", serial_num="EGY1909334569", last_repair_date="7/2/2015", installed_on="6/1/2010", warranty="Expired 6/1/2013").put()
 
-    def clear_datastore(self):
-        models = [Member, Store, Appliance, Provider, WorkOrder, WorkOrderHistory]
-        for model in models:
-            q = model.all(keys_only=True)
-            entries = q.fetch(100)
-            db.delete(entries)
+    #fields['Dispatch Email'], fields['Primary Contact Name'], fields['Phone - Business Hours']
+    def create_provider(self, email, name, phone):
+        provider = Member(key_name = email, name = name, role = 'provider', phone = phone)
+        provider.put()
+        return provider
 
     def create_providers(self):
         Provider(name="Acme Oven Services", location=db.GeoPt(40.719510, -74.008930), owner=Member.get_by_key_name("rajiv@beagleslabs.com"), phone_num="617-840-0716", insurance="Hartford insurance", certifications="Class B Electrician license", reputation=4.0).put()
@@ -70,31 +78,72 @@ class TestDataCreationHandler(WebRequestHandler):
         Provider(name="Oakgrove Oven Services", location=db.GeoPt(40.724043, -74.009989), owner=Member.get_by_key_name("rajiv@beagleslabs.com"), phone_num="617-840-0716", insurance="Hartford insurance", certifications="Class B Electrician license", reputation=4.0).put()
 
     def get(self):
-        self.clear_datastore()
+        clear_datastore()
         self.create_users()
         store_ids = self.create_stores()
         self.create_appliances(store_ids)
         self.create_providers()
 
+def map_address(address):
+    if address == '9107 Mendenhall Court, Columbia, MD 21045':
+        return db.GeoPt(39.186183, -76.826163)
+    elif address == '12011 Guilford Road # 101, Annapolis Junction, MD 20701':
+        return db.GeoPt(39.124529, -76.785755)
+    elif address == '1595 Cabin Branch Drive, Landover, MD 20785':
+        return db.GeoPt(38.913211, -76.905566)
+    elif address == '9100 Yellow Brick Road, Suite H, Baltimore, MD 21237':
+        return db.GeoPt(39.345803, -76.472525)
+
 def pull_airtable_data():
+    tdc = TestDataCreationHandler()
+    [owner, manager] = tdc.create_users()
+    [store] = tdc.create_stores(manager, owner)
+
     payload = {'view': airtable.config['view']}
     encoded_payload = urllib.urlencode(payload)
     headers = {'Authorization': 'Bearer '+airtable.config['api_key'], 'Content-Type': 'application/x-www-form-urlencoded'}
+
     url = 'https://api.airtable.com/v0/'+airtable.config['app_id']+'/Appliances'
     response = urlfetch.fetch(url=url,payload=encoded_payload,method=urlfetch.GET,headers=headers)
     if response.content and 'records' in response.content:
         r = json.loads(response.content)
         appliances = r['records']
         for appliance in appliances:
-            logging.info(appliance['id'])
+            appliance_obj = Appliance()
             fields = appliance['fields']
-            for k, v in fields.iteritems():
-                logging.info(str(k)+' : '+str(v))
-            logging.info(appliance['createdTime'])
-            logging.info('******')
+            if len(fields) <= 0:
+                continue
+            appliance_obj.name = fields['Appliance Name'][0]
+            appliance_obj.serial_num = fields['Serial #']
+            appliance_obj.model = fields['Model #']
+            appliance_obj.manufacturer = fields['Manufacturer'][0]
+            appliance_obj.store = store
+            appliance_obj.put()
+    
+    url = 'https://api.airtable.com/v0/'+airtable.config['app_id']+'/Vendors'
+    response = urlfetch.fetch(url=url,payload=encoded_payload,method=urlfetch.GET,headers=headers)
+    if response.content and 'records' in response.content:
+        r = json.loads(response.content)
+        providers = r['records']
+        for provider in providers:
+            provider_obj = Provider()
+            fields = provider['fields']
+            if len(fields) <= 0:
+                continue
+            provider_obj.name = fields['Vendor Name']
+            provider_obj.phone_num = fields['Phone - Business Hours']
+            dispatch_email = fields['Dispatch Email'] if 'Dispatch Email' in fields else 'rajiv@beagleslabs.com'
+            provider = tdc.create_provider(dispatch_email, fields['Primary Contact Name'], fields['Phone - Business Hours'])
+            provider_obj.owner = provider
+            provider_obj.location = map_address(fields['Address'])
+            provider_obj.insurance = 'Hartford insurance'
+            provider_obj.certifications = 'Class B Electrician license'
+            provider_obj.reputation = 4.0
+            provider_obj.put()
 
 class AirtableDataPullHandler(webapp2.RequestHandler):
     def get(self):
+        clear_datastore()
         deferred.defer(pull_airtable_data)
 
 app = webapp2.WSGIApplication([
